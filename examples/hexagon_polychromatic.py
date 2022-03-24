22dc3a32cb73aba65b4a9e73ad96388c27702ce7
dviladrich95 <daniel.viladrich.h@gmail.com>
2022-03-24 16:28:14 +0100

    works with cuda on windows now

--- examples/hexagon_polychromatic.py
+++ examples/hexagon_polychromatic.py
@@ -1,13 +1,16 @@
 import diffractsim
-diffractsim.set_backend("CPU") #Change the string to "CUDA" to use GPU acceleration
+diffractsim.set_backend("CUDA") #Change the string to "CUDA" to use GPU acceleration
+from cupyx.profiler import benchmark
+import os
+import cupyx
 
 from diffractsim import PolychromaticField,ApertureFromImage, cf, mm, cm
 
 F = PolychromaticField(
     spectrum=2 * cf.illuminant_d65, extent_x=18 * mm, extent_y=18 * mm, Nx=1024, Ny=1024
 )
-
-F.add(ApertureFromImage("./apertures/hexagon.jpg", image_size=(5.6 * mm, 5.6 * mm), simulation = F))
+hexagon_path = os.path.join("examples","apertures","hexagon.jpg")
+F.add(ApertureFromImage(hexagon_path, image_size=(5.6 * mm, 5.6 * mm), simulation = F))
 
 F.propagate(z=80*cm)
 
